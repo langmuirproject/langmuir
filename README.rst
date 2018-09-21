88f90047da0054f08fdae437e9752321f871530b
sigvaldm <sigvaldm@fys.uio.no>
2018-09-21 14:18:11 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,23 @@
+langmuir
+=========
+
+.. image:: https://travis-ci.com/sigvaldm/langmuir.svg?branch=master
+    :target: https://travis-ci.com/sigvaldm/langmuir
+
+.. image:: https://coveralls.io/repos/github/sigvaldm/langmuir/badge.svg?branch=master
+    :target: https://coveralls.io/github/sigvaldm/langmuir?branch=master
+
+.. image:: https://img.shields.io/pypi/pyversions/langmuir.svg
+    :target: https://pypi.org/project/langmuir
+
+Functions for work on Langmuir probe theory
+
+Installation
+------------
+Install from PyPI using ``pip`` (preferred method)::
+
+    pip install langmuir
+
+Or download the GitHub repository https://github.com/sigvaldm/langmuir.git and run::
+
+    python setup.py install
---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+"""
+Copyright 2018 Sigvald Marholm <marholm@marebakken.com>
+
+This file is part of langmuir.
+
+langmuir is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+langmuir is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with langmuir  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from setuptools import setup
+from io import open # Necessary for Python 2.7
+
+with open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='langmuir',
+      version='0.1.0',
+      description='Functions for work on Langmuir probe theory',
+      long_description=long_description,
+      author='Sigvald Marholm',
+      author_email='marholm@marebakken.com',
+      url='https://github.com/sigvaldm/langmuir.git',
+      py_modules=['langmuir'],
+      license='LGPL',
+      classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+        ]
+     )
+
