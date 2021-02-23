00e4dcf8d1c97aa53e6a9c8def783bd6965c11da
sigvaldm <sigvaldm@fys.uio.no>
2021-02-23 16:51:15 +0100

    Fixed setup.py classifier error

--- setup.py
+++ setup.py
@@ -46,8 +46,8 @@ setup(name='langmuir',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9'
         ]
      )
