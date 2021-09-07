fbeb823ff8643d2fdcf6b404bc1f33e4921aa429
Diako Darian <diako.darian@gmail.com>
2021-09-07 23:01:27 +0200

    Additional code to take care of invalid kappa values when alpha is zero. Improved the corresponding test.

--- langmuir/analytical.py
+++ langmuir/analytical.py
@@ -144,7 +144,7 @@ def OML_current(geometry, species, V=None, eta=None, normalization=None):
         charge and temperature and k is Boltzmann's constant.
 
     normalization: 'th', 'thmax', None
-        Wether to normalize the output current by, respectively, the thermal
+        Whether to normalize the output current by, respectively, the thermal
         current, the Maxwellian thermal current, or not at all, i.e., current
         in [A/m].
 
@@ -195,10 +195,12 @@ def OML_current(geometry, species, V=None, eta=None, normalization=None):
     else:
         C = np.sqrt(kappa-1.5)*gamma(kappa-1.)/gamma(kappa-0.5)
         if alpha == 0:
+            assert kappa > 1.5, "Invalid value: kappa must be larger than 3/2."
             D = 1.
             E = 0.
             F = (kappa-1.) / (kappa-1.5)
         else:
+            # For Kappa-Cairns distribution, kappa must be bigger than 3:
             assert kappa > 3., "Invalid value: for alpha > 0, kappa must be larger than 3."
             D = (1. + 24 * alpha * ((kappa - 1.5) ** 2 / ((kappa - 2.) * (kappa - 3.)))) / (
                         1. + 15 * alpha * ((kappa - 1.5) / (kappa - 2.5)))
@@ -252,6 +254,7 @@ def OML_current(geometry, species, V=None, eta=None, normalization=None):
         else:
             C = np.sqrt(kappa - 1.5) * (kappa - .5) / (kappa - 1.0)
             if alpha == 0:
+                assert kappa > 1.5, "Invalid value: kappa must be larger than 3/2."
                 I[indices_p] = (2. / np.sqrt(np.pi)) * I0 * C
                 I[indices_p] *= (eta[indices_p] / (kappa - 1.5)) ** (1. - kappa)
                 I[indices_p] *= hyp2f1(kappa - 1., kappa + .5, kappa, 1. - (kappa - 1.5) / (eta[indices_p]))
