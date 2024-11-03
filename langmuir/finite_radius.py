243ae0a03132fb73d8fc006597d1646d19237384
sigvaldm <sigvald@marebakken.com>
2024-11-03 16:09:17 +0100

    This finally fixes issue #38. It turns out to be caused by a combination of two facts: First, the unstructured interpolation, and second, the fact that I had misunderstood Laframboise's table in the implementation. The gaps in Laframboise's tables are not simply the same as the value to the left, which I first thought, but values he has not computed. This is now represented as nans, and the table can be used either by filling in values for the nans, or by using unstructured interpolation.

--- langmuir/finite_radius.py
+++ langmuir/finite_radius.py
@@ -35,48 +35,84 @@ def finite_radius_current(geometry, species, V=None, eta=None, normalization=Non
                           table=None):
     """
     A current model taking into account the effects of finite radius by
-    interpolating between tabulated normalized currents. The model only
-    accounts for the attracted-species currents (for which eta<0). It does
-    not extrapolate, but returns ``nan`` when the input parameters are outside
-    the domain of the model. This happens when the normalized potential for any
-    given species is less than -25, when kappa is less than 4, when alpha is
+    interpolating between tabulated normalized currents. It does not
+    extrapolate, but returns ``nan`` when the input parameters are outside the
+    domain of the model. This happens when the normalized potential for any
+    given species is more than 25, when kappa is less than 4, when alpha is
     more than 0.2 or when the radius is more than 10 or sometimes all the way
-    up towards 100 (as the distribution approaches Maxwellian). Normally finite
-    radius effects are negligible for radii less than 0.2 Debye lengths (spheres)
-    or 1.0 Debye lengths (cylinders).
+    up towards 100. Normally finite radius effects are negligible for radii
+    less than 0.2 Debye lengths (spheres) or 1.0 Debye lengths (cylinders).
 
-    The model can be based on the following tables, as decided by the ``table``
-    parameter:
+    One of several tables can be interpolated from, as decided by the ``table``
+    argument. Some tables contain values on a regular grid in the parameter
+    space, whereas other tables do not. On a regular grid interpolation will be
+    linear along each dimension (using SciPy's RegularGridInterpolation), which
+    results in faster computation, and in fact, less jagged and more accurate
+    resulting curves. For non-regular grids unstructured interpolation through
+    SciPy's LinearNDInterpolation is used. Available tables are listed below.
+    Before experimenting with non-default settings the user is encouraged to
+    read the relevant background literature, and try to understand how the
+    tables are made.
 
-    - ``'laframboise'``.
+    - ``'laframboise'`` (non-regular)
       The de-facto standard tables for finite radius currents, tables 5c
       and 6c in Laframboise, "Theory of Spherical and Cylindrical Langmuir
       Probes in a Collisionless, Maxwellian Plasma at Rest", PhD Thesis.
-      Covers Maxwellian plasmas only, probe radii ranging from 0 to 100 Debye
-      lengths.
+      Covers Maxwellian plasmas only, and probe radii ranging from 0 to 100
+      Debye lengths. This table is believed to contain the numerically most
+      accurate values, but because the table has many gaps, unstructured linear
+      interpolation is used, which sometimes causes jagged, and unaccurate
+      results. Especially near normalized radii of 7.5 or 15 (spheres) or 30 or
+      40 (cylinders). Consider ``'laframboise regularized'`` instead.
 
-    - ``'darian-marholm uncomplete'``.
-      These tables covers Maxwellian, Kappa, Cairns and Kappa-Cairns
-      distributions for radii ranging from 0.2 Debye lengths (spheres) or
-      1.0 Debye length (cylinders) up to 10 Debye lengths. They are not as
-      accurate as ``'laframboise'`` for pure the Maxwellian, but usually within
-      one or two percent.
+    - ``'laframboise regularized'`` (regular)
+      This fills in the gaps in the ``'laframboise'`` tables by one-dimensional
+      interpolation along the radius axis, and subsequently regular grid
+      interpolation is used. This fixes the issues with unstructured
+      interpolation. It is faster and more accurate than ``'laframboise'``.
+      This table will be used by default for Maxwellian plasmas.
 
-    - ``'darian-marholm'``.
+    - ``'darian-marholm uncomplete'`` (regular)
+      These are the tables reported in Darian, Marholm, Mortensen, Miloch,
+      "Theory and simulations of spherical and cylindrical Langmuir probes in
+      non-Maxwellian plasmas", PPCF, 2019. These tables covers Maxwellian,
+      Kappa, Cairns and Kappa-Cairns distributions for normalized radii ranging
+      from 0.2 Debye lengths (spheres) or 1.0 Debye length (cylinders) up to 10
+      Debye lengths. They do not extend downwards to 0 Debye lengths. They are
+      not as accurate as ``'laframboise'`` for pure Maxwellian distribution,
+      but usually within one or two percent. Consider
+      ``'laframboise-darian-marholm'`` instead.
+
+    - ``'darian-marholm'`` (regular)
       Same as above, but this is complemented by adding analytical values from
-      OML theory, thereby extending the range of valid radii down to zero Debye
-      lengths. In addition, the values for zero potential are replaced by
-      analytical values (i.e. the thermal current), since these are amongst the
-      most inaccurate in the above, and more accurate values can be analytically
-      computed.
+      OML theory for zero radii, thereby extending the range of valid radii
+      down to zero Debye lengths. In addition, the values for zero potential
+      are replaced by exact analytical values (i.e. the thermal current). These
+      are known to be among the most inaccurate in the original
+      ``'darian-marholm'`` tables.
 
-    - ``'laframboise-darian-marholm'``.
-      This replaces the tabulated values for the Maxwellian distribution in
-      ``'darian-marholm'`` with those of Laframboise. Accordingly this table
-      produces the most accurate result available in any situation, and has the
-      widest available parameter domain, with the probe radius gradually
-      increasing from 10 towards 100 Debye lengths as the distribution
-      approaches the Maxwellian.
+    - ``'laframboise-darian-marholm'`` (non-regular)
+      This combines the values from ``'laframboise'`` for Maxwellian
+      distribution, with the values from ``'darian-marholm'`` for
+      non-Maxwellian distributions (i.e., excluding the Maxwellian values in
+      the ``darian-marholm'`` table). As such this table spans the largest
+      parameter domain, with normalized radii from 0 to somewhere between 10 or
+      100, depending on values of the spectral indices. This table suffers from
+      inaccurate extrapolation in the same way as the ``'laframboise'`` table.
+      In addition, normalized radii over 10 migh be unaccurate unless the
+      distribution is nearly Maxwellian. Consider ``'laframboise-darian-marholm
+      regularized'`` instead.
+
+    - ``'laframboise-darian-marholm regularized'`` (regular)
+      This combines the values in ``'darian-marholm'`` for non-Maxwellian
+      distributions with those in ``'laframboise regularized'`` in such a
+      manner that the values form a regular grid. This involves discarding
+      values for certain radii from the ``'laframboise regularized'`` tables
+      which do not exist for ``'darian-marholm'``. In particular normalized
+      radii of 7.5 for spheres, and 1.5, 2.5 and 4 for cylinders. In the
+      vicinity of these radii, the ``'laframboise'`` tables will be more
+      accurate. In addition, normalized radii above 10 are not supported. This
+      is the default tables used for non-Maxwellian plasmas.
 
     Parameters
     ----------
@@ -130,9 +166,9 @@ def finite_radius_current(geometry, species, V=None, eta=None, normalization=Non
 
     if table is None:
         if alpha==0 and kappa==np.inf:
-            table = 'laframboise'
+            table = 'laframboise regularized'
         else:
-            table = 'laframboise-darian-marholm'
+            table = 'laframboise-darian-marholm regularized'
 
     eta = deepcopy(eta)
 
@@ -181,21 +217,21 @@ def finite_radius_current(geometry, species, V=None, eta=None, normalization=Non
 
 def get_fr_lerper(table_name):
     """
-    Returns a linear interpolator. For unstructured data, it will return an
+    Returns a linear interpolator. For non-regular data, it will return an
     LinearNDInterpolator object (triangulates the parameter space). For regular
     data it will return a RegularGridInterpolator object.
     """
 
     table = get_table(table_name)
-    if "unstructured" in table_name:
-        pts = table['points']
-        vals = table['values'].reshape(-1)
-        lerper = LinearNDInterpolator(pts, vals)
-    else:
+    if table['regular']:
         pts = table['points']
         axes = table['axes']
         vals = table['values']
         lerper = RegularGridInterpolator(axes, vals, bounds_error=False, fill_value=np.nan)
+    else:
+        pts = table['points']
+        vals = table['values'].reshape(-1)
+        lerper = LinearNDInterpolator(pts, vals)
 
     return lerper
 
